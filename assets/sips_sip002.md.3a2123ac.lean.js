3a9b1000b6d893f441ec6742f46a4160a7faf612
madeye <madeye@users.noreply.github.com>
2022-06-28 04:40:45 +0000

    deploy: ef7f0c25ddddd2c63bd8b4307906eee702fa7523

---
+++ assets/sips_sip002.md.3a2123ac.lean.js
@@ -0,0 +1,1 @@
+import{_ as e,c as s,o,a as t}from"./app.0e0fc182.js";const f=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"sips/sip002.md","lastUpdated":1656391208000}'),a={name:"sips/sip002.md"},n=t("",19),p=[n];function r(l,c,i,d,u,h){return o(),s("div",null,p)}var g=e(a,[["render",r]]);export{f as __pageData,g as default};
