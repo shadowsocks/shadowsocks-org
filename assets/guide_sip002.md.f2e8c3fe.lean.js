ad4c8f854de7e148e95699984b3a4db5cd3906c7
madeye <madeye@users.noreply.github.com>
2022-08-27 03:12:56 +0000

    deploy: 42a6069e89c9e18be38d85be7222ea14233bfc82

---
+++ assets/guide_sip002.md.f2e8c3fe.lean.js
@@ -0,0 +1,1 @@
+import{_ as e,c as s,o,a as t}from"./app.2d006638.js";const g=JSON.parse('{"title":"SIP002 URI scheme","description":"","frontmatter":{},"headers":[],"relativePath":"guide/sip002.md","lastUpdated":1661569928000}'),a={name:"guide/sip002.md"},n=t("",20),p=[n];function r(c,i,l,d,u,h){return o(),s("div",null,p)}const f=e(a,[["render",r]]);export{g as __pageData,f as default};
